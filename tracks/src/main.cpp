399d07af119aa611d24e8d04572382b18c13d421
Dirk Gorissen <dgorissen@gmail.com>
2021-01-11 21:48:17 +0000

    Got it to work on robot, not 100% about calclation method but seems ok

--- tracks/src/main.cpp
+++ tracks/src/main.cpp
@@ -4,6 +4,8 @@
 #include "config.h"
 #include <ArduinoLog.h>
 #include "motor_utils.h"
+#include "utils.h"
+#include <math.h>
 
 // Motor variables
 Encoder encA(motor_encA1, motor_encA2);
@@ -32,7 +34,9 @@ void setup_rc(){
 
 void setup_logging(){
 	Serial.begin(115200);
-	while(!Serial && !Serial.available()){}
+	// Only useful if you want to catch setup output
+	//while(!Serial && !Serial.available()){}
+
 	// Available levels are:
     // LOG_LEVEL_SILENT, LOG_LEVEL_FATAL, LOG_LEVEL_ERROR, LOG_LEVEL_WARNING, LOG_LEVEL_NOTICE, LOG_LEVEL_TRACE, LOG_LEVEL_VERBOSE
     Log.begin(LOG_LEVEL_VERBOSE, &Serial);
@@ -46,22 +50,16 @@ void setup() {
 	Log.notice("Setup done\n");
 }
 
-void rc_mode(){
-  // look for a good SBUS packet from the receiver
-  if(x8r.read(&rc_input[0], &rc_failSafe, &rc_lostFrame)){
-	// Read the values for each channel
-	const int ch1 = rc_input[0];
-	const int ch2 = rc_input[1];
-	const int ch3 = rc_input[2];
-	const int ch4 = rc_input[3];
-	Log.verbose("receiving: %d, %d, %d, %d\n", ch1, ch2 ,ch3,ch4);
-
-	// Map to a symmetric range
-	int vL = map(ch1, rc_minpwm, rc_maxpwm, -motor_maxpwm, motor_maxpwm);
-	int vR = map(ch3, rc_minpwm, rc_maxpwm, -motor_maxpwm, motor_maxpwm);
-	const int deadband = motor_deadband;
+// Simple manual differential steering where channel 1 controls the left
+// track and channel 3 controls the right track
+void dual_joystick(const int ch1, const int ch2, int &vL, int &vR){
+	// Map to a symmetric pwm range centered over 0
+	vL = map(ch1, rc_minpwm, rc_maxpwm, -motor_maxpwm, motor_maxpwm);
+	vR = map(ch2, rc_minpwm, rc_maxpwm, -motor_maxpwm, motor_maxpwm);
 
 	Log.notice("Mapped RC intput to %d -> %d, %d -> %d\n", ch1, vL, ch2, vR);
+
+	const int deadband = motor_deadband;
 
 	if((-deadband <= vL) && (vL <= deadband)){
 		vL = 0;
@@ -92,8 +90,146 @@ void rc_mode(){
 			directionR = fwd;
 		}
 	}
+}
 
-	Log.notice("set speed to %d, %d\n", vL, vR);
+void single_joystick(const int chx, const int chy, int &vL, int &vR){
+	// Map raw channel input to -1, 1
+	const double joy_x = mapf(chx, rc_minpwm, rc_maxpwm, -1.0, 1.0);
+	const double joy_y = mapf(chy, rc_minpwm, rc_maxpwm, -1.0, 1.0);
+	const double r_max = 1.0;
+
+	// The model is a single joystick that can move in the x (left-right) and y (up-down)
+	// plane. We use a polar coordinate system where, for a particular point (r, theta)
+	// r should represent the speed the robot needs to travel at and theta should represent
+	// the bearing of the robot.
+
+	// References:
+	//  https://robotics.stackexchange.com/questions/7829/how-to-program-a-three-wheel-omni
+	//  https://robotics.stackexchange.com/questions/20347/how-do-i-convert-centre-returning-joystick-values-to-dual-hobby-motor-direction
+	//  https://math.stackexchange.com/questions/553478/how-covert-joysitck-x-y-coordinates-to-robot-motor-speed
+
+	// Convert to polar coordinates
+	const double theta = atan2(joy_y, joy_x);
+	const double r = sqrt(joy_x * joy_x + joy_y * joy_y);
+
+	// Take our polar space the one where r is maximal. So we have a cirlce with diameter r, where r is our max
+	// speed. That means all possible speeds should fall within this circle.
+	// That all sounds great but there is an issue if we stop here. Imagine moving the joystick to the top right corner.
+	// Doing the polar conversion would result in r being equal to sqrt(x^2+y^2) which is larger than the max
+	// we set originally for r. E.g., if we take the max robot speed to be 5 and use that as the radius for our cirlce.
+	// then position the stick at the top right corner, do the polar conversion, you would get sqrt(50) ~= 7.
+	// So the robot would go faster with the stick in top right, than with the stick directly forward.
+	// That is not intuitive. We can solve this as follows:
+
+	// Let point J = (joy_x, joy_y) be the original position of the stick then let OJ be the line segment connecting the origin
+	// with point J. Denote the length of OJ as b. Let C = (x_c, y_c) be the point where the OJ intersects our circle with radius
+	// r. OC thus has length r. if J is on the X-axis or Y-axis then b == r. For all other locations of J outside
+	// of the cirlc we have to scale J to lie on the circle.
+	// 
+	// //------ way 1
+	// // O-J-J_x is a right angle triangle with theta in O
+	// // Find the ratio between the hypotenuse (OJ) and the adjacent side (OJ_X or OJ_Y)
+	// double ratio = -1;
+	// if (abs(joy_x) > abs(joy_y)) {
+	// 	// if you take the top right quadrant, this means that theta is less than 45 degrees
+	// 	// so then the adjacent side is on X
+    // 	ratio = abs(r / joy_x);
+	// } else {
+	// 	// if you take the top right quadrant, this means that theta is more than 45 degrees
+	// 	// so then the adjacent side is on Y
+	// 	ratio = abs(r / joy_y);
+	// }
+
+	// // For example. With the stick at just under 45 degrees, max to the right,
+	// // r will be just under sqrt(2) and joy_x will be 1
+	// // So then ratio will be just under sqrt(2) = ~1.4
+
+	// // Now calculate the actual (scaled) speed factor
+	// // So in the example, speed = sqrt(2) / ~sqrt(2) = >1
+	// const double fac = r/ratio;
+	// // const double turn_damping = 1;
+	// // const double rawL = fac * (sin(theta) + cos(theta) / turn_damping);
+	// // const double rawR = fac * (sin(theta) - cos(theta) / turn_damping);
+
+	// // Calculate the final motor speeds, adding both contributions
+	// const double x_comp = fac * cos(theta);
+	// const double y_comp = fac * sin(theta);
+
+	// const double rawL = y_comp + x_comp;
+	// const double rawR = y_comp - x_comp;
+
+	// Log.notice("chx=%d, chy=%d, joy_x=%D, joy_y=%D, r=%D, theta=%D, ratio=%D, fac=%D, y_comp=%D, x_comp=%D, rawL=%D, rawR=%D\n", chx, chy, joy_x, joy_y, r, theta, ratio, fac, y_comp, x_comp, rawL, rawR);
+	
+	//------ way 2
+	double s = -1;
+	if(r > r_max){
+		s = (r_max / r);
+	} else {
+		s = 1;
+	}
+	const double joy_x_scaled = joy_x * s;
+	const double joy_y_scaled = joy_y * s;
+
+	// Turn into wheel velocities
+	// Ensure between [-1,1], adding scaled components together can still exceed 1
+	// TODO: not 100% happy I understand why this is needed
+	const double rawL = clip(joy_y_scaled + joy_x_scaled, -1, 1);
+	const double rawR = clip(joy_y_scaled - joy_x_scaled, -1, 1);
+	
+	Log.notice("chx=%d, chy=%d, joy_x=%D, joy_y=%D, r=%D, theta=%D, joy_x_scaled=%D, joy_y_scaled=%D, rawL=%D, rawR=%D\n", chx, chy, joy_x, joy_y, r, theta, joy_x_scaled, joy_y_scaled, rawL, rawR);
+	
+	vL = (int) (rawL * 255.0);
+	vR = (int) (rawR * 255.0);
+
+	const int deadband = 30;
+
+	if((-deadband <= vL) && (vL <= deadband)){
+		vL = 0;
+	}else if(vL < -deadband) {
+		vL = abs(vL);
+		if(directionL == fwd){
+			set_motor(left, reverse);
+			directionL = reverse;
+		}
+	} else {
+		if(directionL == reverse){
+			set_motor(left, fwd);
+			directionL = fwd;
+		}
+	}
+
+	if((-deadband <= vR) && (vR <= deadband)){
+		vR = 0;
+	}else if(vR < -deadband) {
+		vR = abs(vR);
+		if(directionR == fwd){
+			set_motor(right, reverse);
+			directionR = reverse;
+		}
+	} else {
+		if(directionR == reverse){
+			set_motor(right, fwd);
+			directionR = fwd;
+		}
+	}
+}
+
+void rc_mode(){
+  // look for a good SBUS packet from the receiver
+  if(x8r.read(&rc_input[0], &rc_failSafe, &rc_lostFrame)){
+	// Read the values for each channel
+	const int ch1 = rc_input[0];
+	const int ch2 = rc_input[1];
+	const int ch3 = rc_input[2];
+	const int ch4 = rc_input[3];
+	//Log.verbose("receiving: %d, %d, %d, %d\n", ch1, ch2 ,ch3,ch4);
+
+	int vL = -999;
+	int vR = -999;
+	//dual_joystick(ch1, ch3, vL, vR);
+	single_joystick(ch4, ch1, vL, vR);
+	
+	Log.notice("Motor speeds calculated as %d, %d\n", vL, vR);
 	set_speed(vL, vR);
 
   } else {
@@ -104,7 +240,8 @@ void rc_mode(){
 	  } else if (rc_lostFrame){
 		  	Log.warning("FRAME LOST\n");
 	  }else{
-		  	Log.warning("No good packet received");
+		  	//Log.warning("No good packet received");
+			delay(100);
 	  }
   }
 }
