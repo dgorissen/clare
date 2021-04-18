268fec772b92b6a7ed4d17af8ca1926163f996ac
Dirk Gorissen <dgorissen@gmail.com>
2021-04-18 11:30:44 +0100

    Working NCS2 in docker and web frontend / ros comms

--- brain/entrypoint.sh
+++ brain/entrypoint.sh
@@ -16,6 +16,8 @@ echo "Givng roscore some time to start"
 sleep 5
 
 # Connect to tracks
+export ROS_HOSTNAME=localhost
+export ROS_MASTER_URI=http://localhost:11311
 rosrun rosserial_python serial_node.py _baud:=115200 _port:="${track_serial_usb}"  2>&1 | tee $LOGS/tracks.txt &
 echo $! > $LOGS/tracks.pid
 
@@ -24,9 +26,11 @@ npm run serve --prefix $CLARE/client > $LOGS/frontend.txt 2>&1 &
 echo $! > $LOGS/frontend.pid
 
 # Start web backend
-/home/dgorissen/.pyenv/shims/python3 $CLARE/server/app.py 2>&1 | tee $LOGS/backend.txt
+/home/dgorissen/.pyenv/shims/python3 $CLARE/server/app.py 2>&1 | tee $LOGS/backend.txt &
 echo $! > $LOGS/backend.pid
 
+# Execute any other command
+exec "$@"
+
 # Keep container running
-# tail -f /dev/null
-# exec "$@"
+tail -f /dev/null
