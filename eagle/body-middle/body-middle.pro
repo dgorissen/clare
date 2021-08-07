e645964c7cbe4636453180af95b506c43456f974
Dirk Gorissen <dgorissen@gmail.com>
2021-08-07 15:14:07 +0100

    New arduino nano pcb for body middle with working code

--- eagle/body-middle/body-middle.pro
+++ eagle/body-middle/body-middle.pro
@@ -1,25 +1,25 @@
 EAGLE AutoRouter Statistics:
 
-Job           : /Users/dirk.gorissen/Dropbox/eagle/projects/clare/body-middle.brd
+Job           : /Users/dirk.gorissen/code/arduino/clare/eagle/body-middle/body-middle.brd
 
-Start at      : 12:34:06 (03/07/2021)
-End at        : 12:34:13 (03/07/2021)
-Elapsed time  : 00:00:06
+Start at      : 23:02:09 (26/07/2021)
+End at        : 23:02:10 (26/07/2021)
+Elapsed time  : 00:00:00
 
-Signals       :    24   RoutingGrid: 6 mil  Layers: 2
-Connections   :    45   predefined:  0 ( 0 Vias )
+Signals       :    21   RoutingGrid: 6 mil  Layers: 2
+Connections   :    48   predefined:  48 ( 0 Vias )
 
-Router memory :   363096
+Router memory :   495708
 
 Passname          : TopRouter     Route Optimize1 Optimize2 Optimize3 Optimize4 Optimize5 Optimize6 Optimize7 Optimize8 Optimize9Optimize10Optimize11Optimize12
 
-Time per pass     :  00:00:03  00:00:00  00:00:00  00:00:00  00:00:01  00:00:00  00:00:00  00:00:01  00:00:00  00:00:00  00:00:00  00:00:01  00:00:00  00:00:00
+Time per pass     :  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00  00:00:00
 Number of Ripups  :         0         0         0         0         0         0         0         0         0         0         0         0         0         0
 max. Level        :         0         0         0         0         0         0         0         0         0         0         0         0         0         0
 max. Total        :         0         0         0         0         0         0         0         0         0         0         0         0         0         0
 
-Routed            :        45        45        45        45        45        45        45        45        45        45        45        45        45        45
-Vias              :         0         0         0         0         0         0         0         0         1         0         0         0         0         0
+Routed            :         0         0         0         0         0         0         0         0         0         0         0         0         0         0
+Vias              :         0         0         0         0         0         0         0         0         0         0         0         0         0         0
 Resolution        :   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %   100.0 %
 
 Final             : 100.0% finished
--- ultrasound/src/main.cpp
+++ ultrasound/src/main.cpp
@@ -1,55 +1,130 @@
 #include <Arduino.h>
 
-// Inspired from https://www.maxbotix.com/documents/Arduino%20Codes/HR-MaxSonars/HR_MaxSonar_PW_Chain.ino
-// https://www.maxbotix.com/Arduino-Ultrasonic-Sensors-085/
+// Ref https://www.maxbotix.com/Arduino-Ultrasonic-Sensors-085/
 
 // Baud rate
-const int BAUD = 115200;
-// If this pin is low then the sensors are turned off
-const int rangingEnablePin = A9;
-// Set to low/high depending on the state of the rangingEnablePin
-const int triggerPin = A10;
+const long BAUD = 115200;
 
-const int numSensors = 3;
-const int pwPins[numSensors] = {A2, A3, A4};
-long pulses[numSensors];
+//--- Pin aliases for convenience
+const int D2 = 2;
+const int D3 = 3;
+const int D4 = 4;
+const int D5 = 5;
+const int D6 = 6;
+
+//--- Ultrasonics
+// If this pin is low then the sensors are turned off
+const int rangingEnablePin = D2;
+// Set to low/high depending on the state of the rangingEnablePin
+const int triggerPin = D3;
+// Number of sensors
+const int numUltraSensors = 3;
+// Pin IDs
+const int ultraPins[numUltraSensors] = {A7, A6, A5};
+// Range values
+int ranges[numUltraSensors];
+
+//--- PIR Sensor
+const int pirPin = D4;
+int pirVal = -1;
+
+//--- Voltage Sensor
+const int voltPin = A2;
+float voltVal = -1.0;
+
+//-- Gas sensors
+const int mqPins[2] = {A4, A3};
+int mqVals[2] = {-1, -1};
 
 void setup () {
   Serial.begin(BAUD);
-  for(int i=0; i < numSensors; ++i){
-    pinMode(pwPins[i], INPUT);
+
+  for(int i=0; i < numUltraSensors; ++i){
+    pinMode(ultraPins[i], INPUT);
+  }
+
+  for(int i=0; i < 2; ++i){
+    pinMode(mqPins[i], INPUT);
+  }
+
+  pinMode(pirPin, INPUT_PULLUP);
+  pinMode(voltPin, INPUT);
+}
+
+void read_ranges(){
+  for(int i=0; i < numUltraSensors; ++i){
+    ranges[i] = analogRead(ultraPins[i]) * 5;
   }
 }
 
-void read_sensors(){
-  // This pin outputs a pulse width representation of the distance
-  // with a scale factor of 1uS per mm. Output range is 300uS for 300-mm to 5000uS for
-  // 5000-mm. Pulse width output is +/- 1% of the serial data sent.
-  for(int i=0; i < numSensors; ++i){
-    pulses[i] = pulseIn(pwPins[i], HIGH);
+void read_gasses(){
+  for(int i=0; i < 2; ++i){
+    mqVals[i] = analogRead(mqPins[i]);
   }
 }
 
-void print_ranges(){     
-  for(int i=0; i < numSensors; ++i){
-    Serial.print(pulses[i]);
+void read_voltage(){
+  int val = analogRead(voltPin);
+  const float R1 = 30000.0;
+  const float R2 = 7500.0;
+  float vOUT = (val * 5.0) / 1023.0;
+  voltVal = vOUT / (R2/(R1+R2));
+}
+
+void read_pir(){
+  pirVal = digitalRead(pirPin);
+}
+
+void print_vals(){
+  char s[50];
+  for(int i=0; i < numUltraSensors; ++i){
+    sprintf(s,"U%d %d", i, ranges[i]);
+    Serial.print(s);
     Serial.print(" ");
   }
-  Serial.println(" ");
+
+  for(int i=0; i < 2; ++i){
+    sprintf(s,"MQ%d %d", i, mqVals[i]);
+    Serial.print(s);
+    Serial.print(" ");
+  }
+
+  Serial.print("P ");
+  Serial.print(pirVal);
+  Serial.print(" V ");
+  Serial.print(voltVal);
+  Serial.println("");
 }
 
-void loop () {
-  bool enable = digitalRead(rangingEnablePin) == HIGH;
-  // TODO: for testing
-  enable = true;
+void loop_test () {
+  // float v = pulseIn(A7, HIGH);
+  float v = analogRead(A7)*5;
+  Serial.print("val=");
+  Serial.print(v);
+  Serial.println();
+  delay(100);
+}
 
-  if(enable){
+
+void loop() {
+  bool enableUltras = digitalRead(rangingEnablePin) == HIGH;
+  // TODO: for testing
+  enableUltras = true;
+
+  if(enableUltras){
     digitalWrite(triggerPin,HIGH);
-    read_sensors();
-    print_ranges();
+    delay(1);
+    // digitalWrite(triggerPin,LOW);
+
+    read_ranges();
+    delay(133);
   } else {
     digitalWrite(triggerPin,LOW);    
   }
 
-  delay(133);
+  read_voltage();
+  read_pir();
+  read_gasses();
+
+  print_vals();
 }
