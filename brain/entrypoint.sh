eb13ab2228f67c31be34743530b6e2e2cea9fb66
Dirk Gorissen <dgorissen@gmail.com>
2021-08-07 18:01:10 +0100

    Log before starting each command, put sleeps in between, and tail rosout

--- brain/entrypoint.sh
+++ brain/entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # service ssh start
 
+set -x
+
 LOCAL_IP=`hostname  -I | cut -f1 -d' '`
 
 CLARE=/home/dgorissen/clare/brain
@@ -16,6 +18,7 @@ mkdir -p $LOGS
 rm $LOGS/*
 
 echo
+echo "* Current directory: " $(pwd)
 echo "* Local IP detected as ${LOCAL_IP}"
 echo "* Logs directory set to ${LOGS}"
 echo
@@ -27,27 +30,40 @@ export ROS_MASTER_URI=http://${LOCAL_IP}:11311
 roscore -v --master-logger-level=info 2>&1 | tee $LOGS/roscore.txt &
 echo $! > $LOGS/roscore.pid
 
-echo "Givng roscore some time to start"
+echo "* Giving roscore some time to start"
 sleep 3
 
+# ID of the current run, generated when roscore is started
+RUN_ID=`rosparam get /run_id`
+ROSOUT_FILE=~/.ros/log/${RUN_ID}/rosout.log
+
 # Connect to tracks
+echo "* Starting rosserial_python for tracks"
 rosrun rosserial_python serial_node.py _baud:=115200 _port:="${tracks_serial}"  2>&1 | tee $LOGS/tracks.txt &
 echo $! > $LOGS/tracks.pid
+sleep 2
 
 # Connect to middle
-python3 ../ultrasound/src/clare_middle_node.py --port ${middle_serial} 2>&1 | tee $LOGS/middle.txt &
+echo "* Starting middle node"
+python3 ${CLARE}/../ultrasound/src/clare_middle_node.py --port ${middle_serial} 2>&1 | tee $LOGS/middle.txt &
 echo $! > $LOGS/middle.pid
+sleep 2
 
 # Start web frontend
+echo "* Starting web frontend"
 npm run serve --prefix $CLARE/client > $LOGS/frontend.txt 2>&1 &
 echo $! > $LOGS/frontend.pid
 
 # Start web backend
+echo "* Starting web backend"
 /home/dgorissen/.pyenv/shims/python3 $CLARE/server/app.py 2>&1 | tee $LOGS/backend.txt &
 echo $! > $LOGS/backend.pid
+sleep 2
 
 # Execute any other command
 exec "$@"
 
 # Keep container running
-tail -f /dev/null
+echo "* Tailing rosout file ${ROSOUT_FILE}"
+tail -f ${ROSOUT_FILE}
+
