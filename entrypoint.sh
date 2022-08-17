2b1b5b14de6f97391fff207a8a240d36d43f932e
Dirk Gorissen <dgorissen@gmail.com>
2022-08-17 13:07:00 +0100

    Fix starting of npm serve and update packages

--- entrypoint.sh
+++ entrypoint.sh
@@ -67,6 +67,8 @@ sleep 2
 
 # Start web frontend
 echo "* Starting web frontend"
+#https://stackoverflow.com/questions/69665222/node-js-17-0-1-gatsby-error-digital-envelope-routinesunsupported-err-os
+export NODE_OPTIONS=--openssl-legacy-provider
 npm run serve --prefix $CLARE/interface/client > $LOGS/frontend.txt 2>&1 &
 echo $! > $LOGS/frontend.pid
 sleep 2
